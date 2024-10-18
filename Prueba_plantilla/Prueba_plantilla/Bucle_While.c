4d5cb5d3c304fed815a879b018b6223dc8eafb0a
boracs <msndeborjabolado@gmail.com>
2024-10-18 09:15:25 +0200

    ejercicio 4, sexo y edad ciclo do_while

---
+++ Prueba_plantilla/Prueba_plantilla/Bucle_While.c
@@ -0,0 +1,49 @@
+
+#include<stdio.h>
+#include<Windows.h>
+
+
+main() {
+
+	SetConsoleCP(1252);
+	SetConsoleOutputCP(1252);
+
+	/*
+	Escribir un programa que pida Edad(int) hasta introducir 0 y sexo(char).Donde si se
+		introduce ‘h’ en sexo será hombre y ‘m’ mujer, validar que solo se introduzcan estos
+		caracteres.Al final mostrar los siguientes datos.
+		Edad de la persona más joven.
+		Edad de la más vieja.
+		Hombres que dieron su edad.
+		Mujeres que dieron su edad.
+
+	*/
+
+
+	printf("Hola");
+
+	int edadMax = 0;
+	int edadMin = 200;
+	int contH = 0;
+	int contM = 0;
+
+	printf("dame tu edad");
+	int edad;
+	scanf_s("%d", &edad);
+
+	while (edad != 0) {
+
+
+
+
+	printf("dame tu edad");
+	int edad;
+	scanf_s("%d", &edad);
+
+	}
+	printf("Sew finalizo el programa");
+
+
+
+}
+
